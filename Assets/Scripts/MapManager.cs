402df7d54c00c404bd80386747b07d960b4f2e81
Xwilarg <xwilarg@yahoo.fr>
2020-03-04 14:52:26 +0100

    Place people on Y axis on plateform

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -32,8 +32,8 @@ public class MapManager : MonoBehaviour
         patterns.Add(new[] { Vector2Int.zero, Vector2Int.right, Vector2Int.right * 2, Vector2Int.right * 3 }); // 4x1
         patterns.Add(new[] { Vector2Int.zero, Vector2Int.right, Vector2Int.right * 2 }); // 2x1
 
-        plateform = new int[5, 10];
-        train = new bool[3, 10];
+        plateform = new int[plateformX, plateformY];
+        train = new bool[trainX, trainY];
         for (var i = 0; i < plateformX * plateformY; i++)
             plateform[i % plateformX, i / plateformY] = 0;
         for (var i = 0; i < trainX * trainY; i++)
@@ -49,12 +49,27 @@ public class MapManager : MonoBehaviour
         while (true)
         {
             Sprite sprite = sprites[Random.Range(0, sprites.Length)];
-            Vector2Int[] pattern = patterns[Random.Range(0, patterns.Count)];
+            List<Vector2Int[]> allPatterns = new List<Vector2Int[]>(patterns);
+            Vector2Int[] pattern;
+            Vector2Int? posOnPlateform;
+            do
+            {
+                if (allPatterns.Count == 0)
+                    throw new System.Exception("No more space on plateform");
+
+                pattern = allPatterns[Random.Range(0, allPatterns.Count)];
+                posOnPlateform = PlaceOnPlateform(pattern);
+                allPatterns.Remove(pattern);
+            } while (!posOnPlateform.HasValue); // Try to find is pattern an be put on plateform
+
+            foreach (var pos in pattern)
+                plateform[pos.x, pos.y] = 1;
+
             GameObject group = new GameObject("Group " + groupNb, typeof(PeopleGroup));
             foreach (Vector2Int pos in pattern)
             {
                 GameObject go = Instantiate(peoplePrefab, group.transform);
-                go.transform.position = (Vector2)pos;
+                go.transform.position = (Vector2)pos + posOnPlateform.Value;
                 go.GetComponent<SpriteRenderer>().sprite = sprite;
             }
 
@@ -64,11 +79,52 @@ public class MapManager : MonoBehaviour
     }
 
     /// <summary>
+    /// Check if a position on the plateform is occupied or not
+    /// <returns></returns>
+    private bool IsPlateformPosFree(int x, int y)
+        => x < plateformX && y < plateformY && plateform[x, y] == 0;
+
+    /// <summary>
+    /// Check if we can put a pattern on the y position of a plateform
+    /// </summary>
+    /// <returns>Return X pos of the pattern or -1 if it doesn't fit</returns>
+    private int DoesPatternFitOnPlateform(int y, Vector2Int[] pattern)
+    {
+        int initX = -1; // X position on the plateform
+        for (int x = 0; x < plateformX; x++) // We find the first free space available on the line Y
+        {
+            if (IsPlateformPosFree(x, y))
+            {
+                initX = x;
+                break;
+            }
+        }
+        if (initX == -1) // No position available for first element of pattern
+            return -1;
+
+        foreach (var elem in pattern)
+        {
+            if (!IsPlateformPosFree(initX + elem.x, y + elem.y))
+                return -1;
+        }
+        return initX;
+    }
+
+    /// <summary>
     /// Attempt to place people on the plateform
     /// </summary>
     /// <returns>The position of the group on the plateform or null if they can't be placed</returns>
-    private Vector2Int? PlaceOnPlateform(Vector2Int pattern)
+    private Vector2Int? PlaceOnPlateform(Vector2Int[] pattern)
     {
-        return null;
+        List<Vector2Int> posAvailable = new List<Vector2Int>(); // All Y pos where we can put the pattern
+        for (int i = 0; i < plateformY; i++) // For each plateform lines
+        {
+            int xPos = DoesPatternFitOnPlateform(i, pattern);
+            if (xPos != -1)
+                posAvailable.Add(new Vector2Int(xPos, i));
+        }
+        if (posAvailable.Count == 0)
+            return null;
+        return posAvailable[Random.Range(0, posAvailable.Count)];
     }
 }
